5971edfb63d05a8fd952cf8aa02e3e85413a7b44
RealDanTheMan <dantheman.py@gmail.com>
2024-12-30 11:49:34 +0000

    Changed order of includes

--- init.lua
+++ init.lua
@@ -1,8 +1,5 @@
 print("Hello Mr Dan, have a great day!")
 
 require("mrdan.keymap")
-require("mrdan.options")
 require("mrdan.lazy")
-
---vim.cmd("colorscheme tokyonight")
-require("colorbuddy").colorscheme("nvim-rdark")
+require("mrdan.options")
