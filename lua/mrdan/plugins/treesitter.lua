57396d7b461c837774190742ace6d4a16a62537e
RealDanTheMan <dantheman.py@gmail.com>
2024-12-30 20:13:52 +0000

    Updated tree sitter plugin config

--- lua/mrdan/plugins/treesitter.lua
+++ lua/mrdan/plugins/treesitter.lua
@@ -3,10 +3,31 @@ return {
 	lazy = false,
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = {"lua", "python", "json", "xml", "go", "rust", "javascript"},
-			highlight = {enabled = true},
+			ensure_installed = {
+				"lua",
+				"python",
+				"json",
+				"xml",
+				"go",
+				"rust",
+				"javascript",
+				"c",
+				"cpp",
+				"c_sharp",
+				"vim",
+				"vimdoc",
+				"css",
+				"rust",
+				"glsl",
+			},
+			highlight = {
+				enabled = true,
+				additional_vim_regex_highlighting = false
+			},
 			indent = {enabled = true},
 			auto_install = true,
+			sync_install = true,
+
 		})
 	end,
 }
