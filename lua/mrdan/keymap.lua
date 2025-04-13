5f38c7ee124f285434557f098cb5aed1136534c4
RealDanTheMan <dantheman.py@gmail.com>
2025-04-13 20:54:38 +0100

    Added hotkesy for symbol renaming and searching

--- lua/mrdan/keymap.lua
+++ lua/mrdan/keymap.lua
@@ -1,4 +1,8 @@
 local vim = vim
 
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", ":Ex<CR>") 
+vim.keymap.set("n", "<leader>e", ":Ex<CR>")
+
+vim.keymap.set("n", "<leader>rr", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references)
+vim.keymap.set("n", "<leader>rd", vim.lsp.buf.definition)
